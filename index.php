46c83181f6a2d919c65bdd3ca0f9e58fddea0806
BLANCASOTO <1715110171@utec-tgo.edu.mx>
2017-07-10 12:12:39 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-
+<!DOCTYPE html>
+<HTML leng="es">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <tittle>ingresa</tittle>
+  </head>
+  <body> 
+    <p>
+      ingresa a un nuevo usuario 
+    </p>
+  </body>
+</HTML>
