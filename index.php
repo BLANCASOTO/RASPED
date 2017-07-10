9bc4c86ff03b352cfa6bb08c1bf475d1ae2ce850
BLANCASOTO <1715110171@utec-tgo.edu.mx>
2017-07-10 13:46:26 -0500

    Update index.php

--- index.php
+++ index.php
@@ -8,14 +8,17 @@
     <title>ingresa</title>
   </head>
   <body background = "https://image.ibb.co/cRhdnv/rasped_con_nombre.jpg">
-ctx.beginPath();
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> //evita zoom
+    var canvas=document.getElementById("canvas");
+    ctx.beginPath();
 ctx.strokeStyle="red";
 ctx.moveTo(100,100);
 ctx.lineTo(150,150);
 ctx.lineTo(100,150);
 ctx.closePath();
 ctx.stroke();
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+var ctx=c.getContext("2d");
     <img src="https://image.ibb.co/cRhdnv/rasped_con_nombre.jpg" class="img-responsive" alt="Imagen responsive">
     <p>
       ingresa a un nuevo usuario 
