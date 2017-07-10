5e4bd31b32d08ae2ada7606d55201656318d730a
BLANCASOTO <1715110171@utec-tgo.edu.mx>
2017-07-10 13:19:29 -0500

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,13 @@
     <title>ingresa</title>
   </head>
   <body background = "https://image.ibb.co/cRhdnv/rasped_con_nombre.jpg">
+ctx.beginPath();
+ctx.strokeStyle="red";
+ctx.moveTo(100,100);
+ctx.lineTo(150,150);
+ctx.lineTo(100,150);
+ctx.closePath();
+ctx.stroke();
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <img src="https://image.ibb.co/cRhdnv/rasped_con_nombre.jpg" class="img-responsive" alt="Imagen responsive">
     <p>
