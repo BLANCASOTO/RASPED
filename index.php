4ae6babf1a4645596423c373c4bb567f5af0c79b
BLANCASOTO <1715110171@utec-tgo.edu.mx>
2017-07-10 12:21:52 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>ingresa</title>
   </head>
-  <body> 
+  <body background = "https://ibb.co/fWtjSv">
+    
     <p>
       ingresa a un nuevo usuario 
     </p>
