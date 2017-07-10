3d87ce4ea9d147023553180ee822072ed1fd8ada
BLANCASOTO <1715110171@utec-tgo.edu.mx>
2017-07-10 14:53:24 -0500

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
          function dibujaForma(){
             // Toma el elemento canvas usando el DOM
             var canvas = document.getElementById('mycanvas');
-           lienzo.fillStyle="rgb(255,0,0)";
+           lienzo.fillStyle="rgb(#26C6DA)";
 
             // Asegurate de no ejecutar el canvas en navegadores que no lo soporten
             if (canvas.getContext){
