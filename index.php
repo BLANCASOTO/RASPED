45fbb00353a7620821e27bb4f02f3174dc3d2cbb
BLANCASOTO <1715110171@utec-tgo.edu.mx>
2017-07-17 17:36:49 -0500

    Update index.php

--- index.php
+++ index.php
@@ -31,7 +31,8 @@ background-size: cover;
       </style>
       <script type="text/javascript">
          function dibujaForma(){
-            // Toma el elemento canvas usando el DOM
+            // Toma el elemento canvas usando el DOm
+           
             var canvas = document.getElementById('mycanvas');
            lienzo.fillStyle="rgb(#26C6DA)";
 
