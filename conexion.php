918dc0f56542c8062363f8edb3730a23850f54c3
BLANCASOTO <1715110171@utec-tgo.edu.mx>
2017-08-07 13:48:57 -0500

    Create conexion.php

---
+++ conexion.php
@@ -0,0 +1,12 @@
+<?php
+	 $server = "bmsyhziszmhf61g1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306";
+	$usuario = "jiwornjvhyvd1y91";
+	$contrasena = "w8o8wmj01z4hw4pz";
+	$db_name = "wkwxn90mtdsft056";
+
+
+
+  //conexión a la base de datos
+  
+
+?>
