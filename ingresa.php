a5829d23a40be02290c5a8bc675dca4693be6d4a
BLANCASOTO <1715110171@utec-tgo.edu.mx>
2017-08-07 13:50:12 -0500

    Create ingresa.php

---
+++ ingresa.php
@@ -0,0 +1,23 @@
+<?php
+	require_once('conexion.php');
+	$conexion=mysql_connect ($servidor,$usuario,$contrasena,$bd_name);
+	$tel = $_REQUEST['tel'];
+	$password = $_REQUEST['password'];
+	$password = md5($password);
+	$query = 
+	"select P.id_personal, contac(t.fk_lada,T.telefono) as telefono, P.contrasena 
+	from personal P, telefonos T
+	where P.fk_telefono = T.id_telefono and
+	$telefono = concat(T.fk_lada_T.telefono) and
+	$contrasena = P.contrasena";
+	$resul_query = mysql_query($conexion,$query);
+	mysql_set_charset ($conexion,"utf8");
+	$registro =  false;
+	$arreglo = array();
+	while (row=myqsl_fetch_array($resul_query))
+		{
+			$registro = true;
+			header("locatiion:/menu.php"); 
+}
+$close = mysql _close($conexion)
+?> 
