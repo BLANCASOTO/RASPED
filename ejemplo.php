ee40589030c5e228a3118b6d173fc03c97e722b2
BLANCASOTO <1715110171@utec-tgo.edu.mx>
2017-08-08 17:44:41 -0500

    Update ejemplo.php

--- ejemplo.php
+++ ejemplo.php
@@ -1,27 +1,24 @@
 <?php
-	require_once ('conexion.php')
+  //se almacenan las variables a insertar
+  $contrasena = $_POST['contrasena'];
+  $id_personal = $_POST['id_personal'];
+  
+  //llamar mysql-login.php que contiene los datos de la base de datos para conectar
+  require_once ('conexion.php');
 
-	$conexion = mysql_connect($server,$user,$pass,$bd);
-
-	$tel = $_REQUEST['tel'];
-	$password = $REQUEST['password'];
-
-	$password = md5($password);
-	
-
-$sql2=mysql_query("
-select P.id_personal, concat(T.fk_lada,T.telefono) as telefono, P.contrasena
-from personal P, telefonos T
-where P.fk_telefono = T.id_telefono;
-
-if ($f2=mysql_fetch_array($sql2)){
-	if($contrasena==$f2['password']){
-	echo '<script>alert("Bienvenido administrador")</script>';
-	echo "<script>"location.href= '/menu.php</scrip'>
-	}
-
-
-
-
-
-
+  //ejecucion de conexion o devolucion de error
+  $conexion = mysqli_connect($server, $user, $pass,$bd);;
+  if (!$conexion) {
+    die('Connect Error: '.mysqli_connect_error());
+  }
+  //estándar de codificación Unicode Transformation 8 bits para compatibilidad ASCII
+  mysqli_set_charset($conexion, "utf8");
+  //consultas - inserccion de cupos y telefonos
+  $query_update_pass= "update personal set contrasena = '$contrasena' where id_personal = '$id_personal'";
+  //ejecucion - inserccion de cupos y telefonos
+  $result_update_pass = mysqli_query($conexion, $query_update_pass) or die('update_pass Error:'.mysqli_error());
+  //cerrar conexion
+  mysqli_close($conexion)or die("Error en desconexion");
+  echo "true";
+  
+?
