c3a198146d0b3aec12cfec5142f2a1b35a0fac6d
BLANCASOTO <1715110171@utec-tgo.edu.mx>
2017-08-08 17:05:21 -0500

    Update ejemplo.php

--- ejemplo.php
+++ ejemplo.php
@@ -9,9 +9,13 @@
 	$password = md5($password);
 	
 
-$sql2=mysql_query("select * from personal where contrasena = $password");
+$sql2=mysql_query("
+select P.id_personal, concat(T.fk_lada,T.telefono) as telefono, P.contrasena
+from personal P, telefonos T
+where P.fk_telefono = T.id_telefono;
+
 if ($f2=mysql_fetch_array($sql2)){
-	if($password==$f2['password']){
+	if($contrasena==$f2['password']){
 	echo '<script>alert("Bienvenido administrador")</script>';
 	echo "<script>"location.href= '/menu.php</scrip'>
 	}
