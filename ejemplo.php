50cc056d1098cbfca90731c0e9a4b862d6e063fd
BLANCASOTO <1715110171@utec-tgo.edu.mx>
2017-08-09 18:42:41 -0500

    Update ejemplo.php

--- ejemplo.php
+++ ejemplo.php
@@ -1,36 +1,75 @@
 <?php
-$conexion = new mysqli($server, $user, $pass, $db;
 
-if ($conexion->connect_error) {
+//carga y se conecta a la base de datos
+require("config.inc.php");
 
- die("La conexion no fue exitosa: " . $conexion->connect_error);
+if (!empty($_POST)) {
+    //obteneos los usuarios respecto a la usuario que llega por parametro
+    $query = "SELECT concat(T.fk_lada,T.telefono) as telefono, P.contrasena
+from personal P, telefonos T
+where P.fk_telefono = T.id_telefono";
+
+    
+    $query_params = array(
+        'telefono' => $_POST['telefono']
+    );
+    
+    try {
+        $stmt   = $db->prepare($query);
+        $result = $stmt->execute($query_params);
+    }
+    catch (PDOException $ex) {
+        //para testear pueden utilizar lo de abajo
+        //die("la consulta murio " . $ex->getMessage());
+        
+        $response["success"] = 0;
+        $response["message"] = "Problema con la base de datos, vuelve a intetarlo";
+        die(json_encode($response));
+        
+    }
+    
+    //la variable a continuación nos permitirará determinar 
+    //si es o no la información correcta
+    //la inicializamos en "false"
+    $validated_info = false;
+    
+    //bamos a buscar a todas las filas
+    $row = $stmt->fetch();
+    if ($row) {
+        //si el password viene encryptado debemos desencryptarlo acá
+        // ++ DESCRYPTAR ++//
+
+        //encaso que no lo este, solo comparamos como acontinuación
+        if ($_POST['P.contrasena'] === $row['P.contrasena']) {
+            $login_ok = true;
+        }
+    }
+    
+    // así como nos logueamos en facebook, twitter etc!
+    // Otherwise, we display a login failed message and show the login form again 
+    if ($login_ok) {
+        $response["success"] = 1;
+        $response["message"] = "Login correcto!";
+        die(json_encode($response));
+    } else {
+        $response["success"] = 0;
+        $response["message"] = "Login INCORRECTO";
+        die(json_encode($response));
+    }
+} else {
+?>
+  <h1>Login</h1> 
+  <form action="index.php" method="post"> 
+      Username:<br /> 
+      <input type="text" name="telefono" placeholder="telefono" /> 
+      <br /><br /> 
+      Password:<br /> 
+      <input type="password" name="P.contrasena" placeholder="P.contrasena" value="" /> 
+      <br /><br /> 
+      <input type="submit" value="Login" /> 
+  </form> 
+  <a href="register.php">Register</a>
+ <?php
 }
 
-$tel = $_POST['tel'];
-$password = $_POST['password'];
-$sql = "select contac(t.fk_lada,T.telefono) as telefono, P.contrasena 
-  from personal P, telefonos T
-  where P.fk_telefono = T.id_telefono and"
-  $telefono = concat(T.fk_lada_T.telefono) and
-  $contrasena = P.contrasena;"
-$result = $conexion->query($sql);
-if ($result->num_rows > 0) {     
- }
-
- $row = $result->fetch_array(MYSQLI_ASSOC);
- if (password_verify($password, $row['password'])) { 
-
-   $_SESSION['loggedin'] = true;
-    $_SESSION['password'] = $contrasena;
-    $_SESSION['start'] = time();
-    $_SESSION['expire'] = $_SESSION['start'] + (5 * 60);
-  echo "Bienvenido! " . $_SESSION['username'];
-
-    echo "<br><br><a href=panel-control.php>Panel de Control</a>"; 
- } else { 
-
-   echo "Username o Password estan incorrectos.";
-echo "<br><a href='menu.html'>Volver a Intentarlo</a
- }
- mysqli_close($conexion);
- ?>
+?> 
