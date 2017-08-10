a1180b385d46c6e60a15f0cbae00a442d34a8ed9
BLANCASOTO <1715110171@utec-tgo.edu.mx>
2017-08-09 19:02:47 -0500

    Update ejemplo.php

--- ejemplo.php
+++ ejemplo.php
@@ -1,75 +1,29 @@
-<?php
+	<?php
+	require_once ('conexion.php')
 
-//carga y se conecta a la base de datos
-require("conexion.php");
+	$conexion = mysql_connect($server,$user,$pass,$bd);
 
-if (!empty($_POST)) {
-    //obteneos los usuarios respecto a la usuario que llega por parametro
-    $query = "SELECT concat(T.fk_lada,T.telefono) as telefono, P.contrasena
+	$tel = $_REQUEST['telefono'];
+	$password = $REQUEST['P.contrasena'];
+
+	$password = md5($password);
+	$query = "SELECT  concat(T.fk_lada,T.telefono) as telefono, P.contrasena
 from personal P, telefonos T
-where P.fk_telefono = T.id_telefono";
+where P.fk_telefono = T.id_telefono
+";
 
-    
-    $query_params = array(
-        'telefono' => $_POST['telefono']
-    );
-    
-    try {
-        $stmt   = $db->prepare($query);
-        $result = $stmt->execute($query_params);
-    }
-    catch (PDOException $ex) {
-        //para testear pueden utilizar lo de abajo
-        //die("la consulta murio " . $ex->getMessage());
-        
-        $response["success"] = 0;
-        $response["message"] = "Problema con la base de datos, vuelve a intetarlo";
-        die(json_encode($response));
-        
-    }
-    
-    //la variable a continuación nos permitirará determinar 
-    //si es o no la información correcta
-    //la inicializamos en "false"
-    $validated_info = false;
-    
-    //bamos a buscar a todas las filas
-    $row = $stmt->fetch();
-    if ($row) {
-        //si el password viene encryptado debemos desencryptarlo acá
-        // ++ DESCRYPTAR ++//
+	$resul_query = mysql_query($conexion,$query);
+	mysql_set_charset ($conexion,"utf8");
 
-        //encaso que no lo este, solo comparamos como acontinuación
-        if ($_POST['P.contrasena'] === $row['P.contrasena']) {
-            $login_ok = true;
-        }
-    }
-    
-    // así como nos logueamos en facebook, twitter etc!
-    // Otherwise, we display a login failed message and show the login form again 
-    if ($login_ok) {
-        $response["success"] = 1;
-        $response["message"] = "Login correcto!";
-        die(json_encode($response));
-    } else {
-        $response["success"] = 0;
-        $response["message"] = "Login INCORRECTO";
-        die(json_encode($response));
-    }
-} else {
-?>
-  <h1>Login</h1> 
-  <form action="index.php" method="post"> 
-      Username:<br /> 
-      <input type="text" name="telefono" placeholder="telefono" /> 
-      <br /><br /> 
-      Password:<br /> 
-      <input type="password" name="P.contrasena" placeholder="P.contrasena" value="" /> 
-      <br /><br /> 
-      <input type="submit" value="Login" /> 
-  </form> 
-  <a href="menu.php">Register</a>
- <?php
+	$registro =  false;
+	$arreglo = array();
+	while (row=myqsl_fetch_array($resul_query))
+		{
+			$registro = true;
+			header("location:/menu.php"); 
 }
-
+$close = mysql _close($conexion)
 ?> 
+
+	
+
