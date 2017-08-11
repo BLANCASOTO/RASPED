e05c58777383bfa92f600260f6d31547da96b638
BLANCASOTO <1715110171@utec-tgo.edu.mx>
2017-08-11 16:38:36 -0500

    Update ingresa.php

--- ingresa.php
+++ ingresa.php
@@ -1,11 +1,3 @@
-?php
-  //conexion
-  $server = "bmsyhziszmhf61g1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306";
-  $user = "jiwornjvhyvd1y91";
-  $pass = "w8o8wmj01z4hw4pz";
-  $bd = "wkwxn90mtdsft056";
-?>
-
 	<?php
 	require_once ('conexion.php')
 
@@ -13,7 +5,7 @@
 
 if ($conexion->connect_error) {
 
- die("La conexion falló: " . $conexion->connect_error);
+ die("La conexion falló: "$conexion->connect_error);
 
 }
 
