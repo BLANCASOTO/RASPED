4085b1f174d7c8f4e24eb295074274be52db3e1d
BLANCASOTO <1715110171@utec-tgo.edu.mx>
2017-08-11 16:59:30 -0500

    Create miguel.php

---
+++ miguel.php
@@ -0,0 +1,9 @@
+ <form action="index.php" method="post"> 
+      Username:<br /> 
+      <input type="text" name="telefono" placeholder="telefono" /> 
+      <br /><br /> 
+      Password:<br /> 
+      <input type="password" name="P.contrasena" placeholder="P.contrasena" value="" /> 
+      <br /><br /> 
+      <input type="submit" value="Login" /> 
+  </form> 
