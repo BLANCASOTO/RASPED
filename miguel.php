f43615ff812c692c659fc52a786f67bb7610b89d
BLANCASOTO <1715110171@utec-tgo.edu.mx>
2017-08-11 17:05:23 -0500

    Update miguel.php

--- miguel.php
+++ miguel.php
@@ -1,9 +1,35 @@
- <form action="index.php" method="post"> 
-      Username:<br /> 
-      <input type="text" name="telefono" placeholder="telefono" /> 
-      <br /><br /> 
-      Password:<br /> 
-      <input type="password" name="P.contrasena" placeholder="P.contrasena" value="" /> 
-      <br /><br /> 
-      <input type="submit" value="Login" /> 
-  </form> 
+<?php
+
+//carga y se conecta a la base de datos
+require("conexion.php");
+
+if (!empty($_POST)) {
+    //obteneos los usuarios respecto a la usuario que llega por parametro
+    $query = "SELECT concat(T.fk_lada,T.telefono) as telefono, P.contrasena
+from personal P, telefonos T
+where P.fk_telefono = T.id_telefono";
+
+    
+    $query_params = array(
+        'telefono' => $_POST['telefono']
+    );
+    
+    try {
+        $stmt   = $db->prepare($query);
+        $result = $stmt->execute($query_params);
+    }
+    catch (PDOException $ex) {
+        //para testear pueden utilizar lo de abajo
+        //die("la consulta murio " . $ex->getMessage());
+        
+        $response["success"] = 0;
+        $response["message"] = "Problema con la base de datos, vuelve a intetarlo";
+        die(json_encode($response));
+        
+    }
+    
+    //la variable a continuación nos permitirará determinar 
+    //si es o no la información correcta
+    //la inicializamos en "false"
+    $validated_info = false;
+    
