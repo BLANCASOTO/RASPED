6b85829e886cd2959e249493b631ea1a55273a1c
BLANCASOTO <1715110171@utec-tgo.edu.mx>
2017-08-11 11:50:02 -0500

    Update ejemplo.php

--- ejemplo.php
+++ ejemplo.php
@@ -8,14 +8,13 @@ $conexion = mysqli_connect($server,$user,$pass,$bd) or die("error conexion");
 $telefono = $_REQUEST['tel'];
 $contrasena = $_REQUEST['contrasena'];
 
-
 //generamos la consulta
 $contrasena = md5($contrasena);
 
 $query = "select concat(T.fk_lada,T.telefono) as telefono, P.contrasena
 from personal P, telefonos T
-where P.fk_telefono = T.id_telefono";
 
+where P.fk_telefono = T.id_telefono";
 
 //formato de datos utf8 (espanol)
 mysqli_set_charset($conexion,"utf8");
@@ -29,29 +28,28 @@ $arraydatos = array();
 //ciclo while para extraer los datos y almacenarlos en el arreglo
 while($row = mysqli_fetch_array($result)){
 
-
 //concentracion de registros por columna 
+ 
 $telefono=$row['telefono'];
 $contrasena=$row['contrasena']
- 
 
-
-  
-  
-  
 //poblacion del arreglo
 $arraydatos[]=array('telefono'=>$telefono,'contrasena'=>$contrasena);
 
 }//while
 
- echo = $telfono;
-echo = $contrasena;
+/*
+			$registro = true;
+			header("location:/menu.php"); 
+*/
+
+
+ 
 //cierre de conexion
 $close = mysqli_close($conexion) or die("error en desconexion");
 
 //creacion e impresion del objeto JSON
 $json = json_encode($arraydatos);
 echo $json;
-
 
 ?>
