ad84be5fa5d610eb225de0f937abf1221bcb46e2
BLANCASOTO <1715110171@utec-tgo.edu.mx>
2017-08-11 12:37:06 -0500

    Update ejemplo.php

--- ejemplo.php
+++ ejemplo.php
@@ -33,7 +33,9 @@ while($row = mysqli_fetch_array($result)){
 $telefono=$row['telefono'];
 $contrasena=$row['contrasena'];
 
-/*header = ("location:/menu.php"); */	
+	
+	
+header = ("/menu.php"); 	
 	
 //poblacion del arreglo
 $arraydatos[]=array('telefono'=>$telefono,'contrasena'=>$contrasena);
