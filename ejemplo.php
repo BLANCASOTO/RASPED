75b262cff7e99bf9b89c45e4e0790fba048f4864
BLANCASOTO <1715110171@utec-tgo.edu.mx>
2017-08-11 12:08:20 -0500

    Update ejemplo.php

--- ejemplo.php
+++ ejemplo.php
@@ -33,7 +33,7 @@ while($row = mysqli_fetch_array($result)){
 $telefono=$row['telefono'];
 $contrasena=$row['contrasena'];
 
-/*header("location:/menu.php"); 	
+/*header("location:/menu.php"); */	
 	
 //poblacion del arreglo
 $arraydatos[]=array('telefono'=>$telefono,'contrasena'=>$contrasena);
@@ -42,7 +42,7 @@ $arraydatos[]=array('telefono'=>$telefono,'contrasena'=>$contrasena);
 
 			
 
-
+/*
  
 //cierre de conexion
 $close = mysqli_close($conexion) or die("error en desconexion");
