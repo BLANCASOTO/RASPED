d5641e420ac26555a4432345f7910b9d57668412
BLANCASOTO <1715110171@utec-tgo.edu.mx>
2017-08-14 16:23:02 -0500

    Update menu.php

--- menu.php
+++ menu.php
@@ -31,7 +31,7 @@ body {
 <div>
 <table style="width:100%">
       <tr>
-   <td align="center"><div  log"><a href="file:///C:/Users/BlancaSoto/Desktop/RASPED/agregausuario.html"><img src="https://image.ibb.co/b3gMGa/agregar.png"Editar</a></div></td>
+   <td align="center"><div  log"><a href="agregausuario.html"><img src="https://image.ibb.co/b3gMGa/agregar.png"Editar</a></div></td>
   <td align="center"><div  log"><a href="file:///C:/Users/BlancaSoto/Desktop/RASPED/editar.html"><img src="https://image.ibb.co/cFyPma/editar.png"reportes</a></div></td>
   <td align="center"><div  log"><a href="file:///C:/Users/BlancaSoto/Desktop/RASPED/eliminarusuarios.html"><img src="https://image.ibb.co/ihEYCF/borrar.png"borrar</a></div></td>
 <td align="center"><div  log"><a href="file:///C:/Users/BlancaSoto/Desktop/RASPED/reportegeneral.html"><img src="https://image.ibb.co/j6bJUv/buscar.png"Editar</a></div></td>
