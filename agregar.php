2973acac14791582d5a64433f8ef996b23b48b73
BLANCASOTO <1715110171@utec-tgo.edu.mx>
2017-08-14 16:39:30 -0500

    Update agregar.php

--- agregar.php
+++ agregar.php
@@ -5,37 +5,40 @@
 <style>
 body {
     background-color:#006064;
-<div
-table>
-  <tr>
-    <th>Firstname</th>
-    <th>Lastname</th>
-  </tr>
-  <tr>
-    <td>Peter</td>
-    <td>Griffin</td>
-  </tr>
-  <tr>
-    <td>Lois</td>
-    <td>Griffin</td>
-  </tr>
-</table>
 
-</body>
-</html>
-
-</DIV>
+	}
 
 	
-<div class="rasped2">TEXT
-  </br> 
-  text
-  </div>
-}
 
 </style>
 </head>
 <body>
+	
+<form method="post" action="add_reg.php3">
+nombre_personal  :<input type="Text" name="nombre_personal"><br>
+apellido_m:<input type="Text" name="apellido_m"><br>
+apellido_p :<input type="Text" name="apellido_p"><br>
+lada:<input type="Text" name="lada"><br>
+telefono:<input type="int" name="telefono"><br>
+sede:<input type="Text" name="sede"><br>
+cupo:<input type="Text" name="cupo"><br>
+tipo_usuario:<input type="Text" name="tipo_usuario"><br>
+<input type="Submit" name="enviar" value="Agregar integrante">
+</form>
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 <img src="https://image.ibb.co/h9rcNF/tarjetas.jpg" alt="Mountain View" style="float:right;width:30%;height:30%;"
 <p style="text-align: float:left; "></p>
 </body>
