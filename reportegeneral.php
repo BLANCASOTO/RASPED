ef025bbbbb347375b84dd466239e64a61b2d6496
BLANCASOTO <1715110171@utec-tgo.edu.mx>
2017-08-15 14:36:28 -0500

    Update reportegeneral.php

--- reportegeneral.php
+++ reportegeneral.php
@@ -1,6 +1,3 @@
-?php
-reportediario.php
-?>
 <!DOCTYPE html>
 <html>
 
@@ -25,5 +22,43 @@ body {
 <MARQUEE BEHAVIOR=alternate>
 <p style="text-align: left; "> <img src="https://image.ibb.co/jKFd9k/RASPED_BM_icono_Tarjetas.png " alt="imagen 3" width="25%">
 </MARQUEE>
+    
+    
+    <?php
+   require_once ('conexion.php');
+      // conexión
+   $conexion = mysqli_connect($server, $user, $pass,$bd) 
+   or die("Error de conexion  de la base de datos");
+    
+   <table>
+  <tr>
+    <th>Dia</th>
+    <th>Nombre</th> 
+    <th>Hora</th>
+  </tr>
+
+   //consulta
+   $sql = "select P.id_personal, concat(C.fk_sede,C.cupo) as cupo, R.id_retardos, P.nombre_personal, P.apellido_m, P.apellido_p,
+   F.fecha, R.hr_entrada, R.hr_comida_i, R.hr_comida_f, R.hr_salida
+   from personal P, retardos R, fechas F, cupos C
+   where R.fk_personal = P.id_personal and
+   R.fk_fecha = F.id_fecha and
+   P.fk_cupo = C.id_cupo;";
+   mysqli_set_charset($conexion, "utf8"); 
+   
  
-</html>
+ 	$result_query = mysqli_query($conexion, $sql)) die();  
+   while($row = mysqli_fetch_array($result)) { 
+      
+      $Dia = $row['Dia'];
+      $Nombre = $row['Nombre'];
+      $Hora = $row['Hora'];
+      
+      echo"<tr>";
+      echo "<tr>.$Dia.</th>";
+      echo"<tr>.$Nombre.</th>";
+      echo "<tr>.$Hora.</th>";
+      echo"</tr>"
+          ?>
+         </table>
+   </html>
