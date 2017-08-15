e523e9972cb60b2726b61a4c9e1bf121ba1fa85a
BLANCASOTO <1715110171@utec-tgo.edu.mx>
2017-08-15 17:59:47 -0500

    Update reportegeneral.php

--- reportegeneral.php
+++ reportegeneral.php
@@ -1,55 +1,28 @@
-<?php
-   
-   require_once ('conexion.php');
-   $result;
-   
-   // conexión
-   $conexion = mysqli_connect($server, $user, $pass,$bd) 
-   or die("error en la base de datos");
-   
-   //consulta
-   $sql = "select P.id_personal, concat(C.fk_sede,C.cupo) as cupo, R.id_retardos, P.nombre_personal, P.apellido_m, P.apellido_p,
-   F.fecha, R.hr_entrada, R.hr_comida_i, R.hr_comida_f, R.hr_salida
-   from personal P, retardos R, fechas F, cupos C
-   where R.fk_personal = P.id_personal and
-   R.fk_fecha = F.id_fecha and
-   P.fk_cupo = C.id_cupo;";
+<!DOCTYPE html>
+<html>
 
-   $result = $conn->query($sql);
-   $rows = $result->fetchAll();
-
-  <!DOCTYPE html>
- <html lang="en">
 <head>
+<style>
 
-   <meta charset="utf-8"/>
-   <title>Query data sending on ID</title>
-</head>   
-  
- <body>
-      <table border="1">
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>Nombre</th>
-        <th>Email</th>
-        <th>Telefono</th>
-      </tr>
-    </thead>
-    <tbody>
-    <?php
-      foreach ($rows as $row) {
-    ?>
-      <tr>
-      <td><a href="menu.php?id=<?php echo $row['id_personal']; ?>"><?php echo $row['id_personal']; ?></a></td>
-      <td><?php echo $row['dia']; ?></td>
-        <td><?php echo $row['nombre']; ?></td>
-        <td><?php echo $row['hora']; ?></td>
-      </tr>
-    <?php } 
-?>
-    </tbody>
-  </table>
+body {
+    background-color: 	#00bc97;	
+
+}
+</style>
+
+<CENTER>
+<MARQUEE    WIDTH=40% BGCOLOR=#B2DFDB BEHAVIOR=alternate> <FONT FACE=arial COLOR=#9E9E9E SIZE=6> Reporte diario
+</CENTER>
+</MARQUEE> 
+
+
+
+</head>
+<body>
+<MARQUEE BEHAVIOR=alternate>
+<p style="text-align: left; "> <img src="https://image.ibb.co/jKFd9k/RASPED_BM_icono_Tarjetas.png " alt="imagen 3" width="25%">
+</MARQUEE>
+ 
 </html>
 
 
