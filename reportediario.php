ef8c56c9486457b214e158b7f3de759247de11d3
BLANCASOTO <1715110171@utec-tgo.edu.mx>
2017-08-15 14:25:14 -0500

    Update reportediario.php

--- reportediario.php
+++ reportediario.php
@@ -1,39 +1,68 @@
+  <table>
+  <tr>
+    <th>Dia</th>
+    <th>Nombre</th> 
+    <th>Hora</th>
+  </tr>
+ 
 <?php
-	
-	require_once ('conexion.php');
-	
-	// conexión
-	$conexion = mysqli_connect($server, $user, $pass,$bd) 
-	or die("Ha sucedido un error inexperado en la conexion de la base de datos");
-	
-	//consulta
-	$sql = "select P.id_personal, concat(C.fk_sede,C.cupo) as cupo, R.id_retardos, P.nombre_personal, P.apellido_m, P.apellido_p,
-	F.fecha, R.hr_entrada, R.hr_comida_i, R.hr_comida_f, R.hr_salida
-	from personal P, retardos R, fechas F, cupos C
-	where R.fk_personal = P.id_personal and
-	R.fk_fecha = F.id_fecha and
-	P.fk_cupo = C.id_cupo;";
-	mysqli_set_charset($conexion, "utf8"); //formato de datos utf8
-	
-	//ejecución de la consulta
-	if(!$result = mysqli_query($conexion, $sql)) die();
-	
-	//se crea un array
-	$clientes = array();
-	
-	//recoleccion de datos mediante un ciclo while(mientras haya registros)
-	while($row = mysqli_fetch_array($result)) { 
-	    $id_personal=$row['id_personal'];
-	    $cupo=$row['cupo'];
-	    $id_retardos=$row['id_retardos'];
-	    $nombre_personal=$row['nombre_personal'];
-	    $apellido_m=$row['apellido_m'];
-	    $apellido_p=$row['apellido_p'];
-	    $fecha=$row['fecha'];
-	    $hr_entrada=$row['hr_entrada'];
-	    $hr_comida_i=$row['hr_comida_i'];
-	    $hr_comida_f=$row['hr_comida_f'];
-	    $hr_salida=$row['hr_salida'];
-	    $clientes[] = array('id_personal'=> $id_personal,'cupo'=>$cupo, 'id_retardos'=> $id_retardos, 'nombre_personal'=> $nombre_personal, 'apellido_m'=> $apellido_m, 'apellido_p'=> $apellido_p,
-	                        'fecha'=> $fecha, 'hr_entrada'=> $hr_entrada, 'hr_comida_i'=> $hr_comida_i, 'hr_comida_f'=> $hr_comida_f, 'hr_salida'=> $hr_salida,  );
-	}
+   require_once ('conexion.php');
+      // conexión
+   $conexion = mysqli_connect($server, $user, $pass,$bd) 
+   or die("Error de conexion  de la base de datos");
+   
+   //consulta
+   $sql = "select P.id_personal, concat(C.fk_sede,C.cupo) as cupo, R.id_retardos, P.nombre_personal, P.apellido_m, P.apellido_p,
+   F.fecha, R.hr_entrada, R.hr_comida_i, R.hr_comida_f, R.hr_salida
+   from personal P, retardos R, fechas F, cupos C
+   where R.fk_personal = P.id_personal and
+   R.fk_fecha = F.id_fecha and
+   P.fk_cupo = C.id_cupo;";
+   mysqli_set_charset($conexion, "utf8"); 
+   
+ 
+ 	$result_query = mysqli_query($conexion, $sql)) die();  
+   while($row = mysqli_fetch_array($result)) { 
+      
+      $Dia = $row['Dia'];
+      $Nombre = $row['Nombre'];
+      $Hora = $row['Hora'];
+      
+      echo"<tr>";
+      echo "<tr>.$Dia.</th>";
+      echo"<tr>.$Nombre.</th>";
+      echo "<tr>.$Hora.</th>";
+      echo"</tr>"
+          ?>
+         </table>
+     
+  // conexión
+   $conexion = mysqli_connect($server, $user, $pass,$bd) 
+   or die("Error de conexion  de la base de datos");
+   
+   //consulta
+   $sql = "select P.id_personal, concat(C.fk_sede,C.cupo) as cupo, R.id_retardos, P.nombre_personal, P.apellido_m, P.apellido_p,
+   F.fecha, R.hr_entrada, R.hr_comida_i, R.hr_comida_f, R.hr_salida
+   from personal P, retardos R, fechas F, cupos C
+   where R.fk_personal = P.id_personal and
+   R.fk_fecha = F.id_fecha and
+   P.fk_cupo = C.id_cupo;";
+   mysqli_set_charset($conexion, "utf8"); 
+   
+ 
+ 	$result_query = mysqli_query($conexion, $sql)) die();  
+   while($row = mysqli_fetch_array($result)) { 
+      
+      $Dia = $row['Dia'];
+      $Nombre = $row['Nombre'];
+      $Hora = $row['Hora'];
+      
+      echo"<tr>";
+      echo "<tr>.$Dia.</th>";
+      echo"<tr>.$Nombre.</th>";
+      echo "<tr>.$Hora.</th>";
+      echo"</tr>"
+          ?>
+         </table>
+     
+           
