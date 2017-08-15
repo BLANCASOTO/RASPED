f29eb9471f3bf0b2e67d638f6890d6e849749646
BLANCASOTO <1715110171@utec-tgo.edu.mx>
2017-08-15 12:49:07 -0500

    Update reportegeneral.php

--- reportegeneral.php
+++ reportegeneral.php
@@ -43,8 +43,9 @@ table#t01 th {
 <MARQUEE    WIDTH=40% BGCOLOR=#B2DFDB BEHAVIOR=alternate> <FONT FACE=arial COLOR=#9E9E9E SIZE=6> Reporte diario
 </CENTER>
 </MARQUEE> 
-<MARQUEE BEHAVIOR=alternate>
+
     </MARQUEE>
+    <MARQUEE BEHAVIOR=alternate>
 <p style="text-align: left; "> <img src="https://image.ibb.co/jKFd9k/RASPED_BM_icono_Tarjetas.png " alt="imagen 3" width="25%">
 </MARQUEE>
 
