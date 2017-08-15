46e519ed8f5acfb97eb305822d4b521f5ba1762b
BLANCASOTO <1715110171@utec-tgo.edu.mx>
2017-08-15 17:53:19 -0500

    Update reportegeneral.php

--- reportegeneral.php
+++ reportegeneral.php
@@ -1,8 +1,8 @@
 require_once ('conexion.php');
-$q = "SELECT * FROM Tabla1 ORDER BY campo1 DESC";
+$q = "SELECT * FROM personal ORDER BY nombre_personal DESC";
 $rs = mysql_query($q);
 while ($row = mysql_fetch_array($rs)) {
-		echo '<p>'.$row['campo1'].'</p>';
+		echo '<p>'.$row['nombre_personal'].'</p>';
 }
 Desconectar($link);
 
