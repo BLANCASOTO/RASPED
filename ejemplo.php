64d9b7afe541c23c6f13c5c9cd63519adcad84b9
BLANCASOTO <1715110171@utec-tgo.edu.mx>
2017-08-16 16:58:11 -0500

    Update ejemplo.php

--- ejemplo.php
+++ ejemplo.php
@@ -20,8 +20,9 @@ where P.fk_telefono = T.id_telefono"
 mysqli_set_charset($conexion,"utf8");
 
 //ejecutar la consulta
-if(!$result = mysqli_query($conexion, $query)) die();
-
+if(!$result = mysqli_query($conexion, $query)){ 
+	die();
+}
 
 
 //creacion del array contenedor de registros
