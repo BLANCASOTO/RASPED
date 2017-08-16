7cc6436a82ddc7c31919ebee534b6aa5c837ea9d
BLANCASOTO <1715110171@utec-tgo.edu.mx>
2017-08-16 12:14:15 -0500

    Update agrega.php

--- agrega.php
+++ agrega.php
@@ -1,8 +1,6 @@
 <?php
   //se almacenan las variables a insertar
-
- 
-
+  
   $nombre_personal = $_POST['nombre_personal'];
   $apellido_m = $_POST['apellido_m'];
   $apellido_p = $_POST['apellido_p'];
@@ -15,8 +13,8 @@
   $puesto = $_POST['puesto']; 
   $tipo = $_POST['tipo']; 
 $fk_horario = '';
- 
- //llamar mysql-login.php que contiene los datos de la base de datos para conectar
+  
+  //llamar mysql-login.php que contiene los datos de la base de datos para conectar
   require_once ('conexion.php');
   //ejecucion de conexion o devolucion de error
   $conexion = mysqli_connect($server, $user, $pass,$bd);;
@@ -25,18 +23,15 @@ $fk_horario = '';
   }
   //estándar de codificación Unicode Transformation 8 bits para compatibilidad ASCII
   mysqli_set_charset($conexion, "utf8");
-
   //consultas - inserccion de cupos y telefonos
-  $query_insert_nombre_personal = "insert ignore into personal(nombre_personal) values ('$nombre_personal')";
   $query_insert_sede = "insert ignore into sedes(id_sede) values ('$sede')";
   $query_insert_cupo = "insert into cupos(fk_sede,cupo) values ('$sede','$cupo')";
-  $query_insert_lada = "insert ignore into ladas(id_lada) values ('$lada')"; 
+  $query_insert_lada = "insert ignore into ladas(id_lada) values ('$lada')";
   $query_insert_telefono = "insert into telefonos(fk_lada,telefono) values ('$lada','$telefono')";
   //ejecucion - inserccion de cupos y telefonos
-  $result_insert_nombre_personal = mysqli_query($conexion,$query_insert_nombre_personal) or die ('result_insert_nombre_personal':'.errorennombre());
-  $result_insert_sede = mysqli_query($conexion, $query_insert_sede) or die('result_insert_sede Error:'.mysqli_error());
-  $result_insert_cupo = mysqli_query($conexion, $query_insert_cupo) or die('result_insert_cupo Error:'.mysqli_error());
-  $result_insert_lada = mysqli_query($conexion, $query_insert_lada) or die('result_insert_lada Error:'.mysqli_error());
+  $result_insert_sede = mysqli_query($conexion, $query_insert_sede) or die('result_insert_sede Error1:'.mysqli_error1());
+  $result_insert_cupo = mysqli_query($conexion, $query_insert_cupo) or die('result_insert_cupo Error2:'.mysqli_error());
+  $result_insert_lada = mysqli_query($conexion, $query_insert_lada) or die('result_insert_lada Error3:'.mysqli_error());
   $result_insert_telef = mysqli_query($conexion, $query_insert_telefono) or die('result_insert_telef Error4:'.mysqli_error());
   //encriptacion md5 de contraseña
 
@@ -54,7 +49,7 @@ echo "<strong>El número aleatorio generado en PHP es:</strong> ".$numeroAleator
   
  
   $contrasena = md5($contrasena);
-$LetraAleatora,$numeroAleatorio=($contrasena)
+
   //query para consulta de fk_cupo y fk_telefono
   $query_select_fk_cupo = "SELECT id_cupo from cupos WHERE fk_sede = $sede and cupo = $cupo";
   $query_select_fk_telefono = "SELECT id_telefono from telefonos WHERE fk_lada = $lada and telefono = $telefono";
@@ -86,7 +81,7 @@ $LetraAleatora,$numeroAleatorio=($contrasena)
   
   //query para la inserccion de usuario
   $query_insert_personal = "insert into personal(fk_cupo,nombre_personal,apellido_m,apellido_p, fk_telefono,contrasena,fk_horario,fk_puesto,fk_usuario) values('$fk_cupo','$nombre_personal','$apellido_m','$apellido_p','$fk_telefono','$contrasena','$fk_horario','$fk_puesto','$tipo')";
-  $result = mysqli_query($conexion, $query_insert_personal) or die('result_ Errorentodo:'.mysqli_error());
+  $result = mysqli_query($conexion, $query_insert_personal) or die('result_ Error:'.mysqli_error());
   
   mysqli_close($conexion)or die("Error en desconeccion");
   
