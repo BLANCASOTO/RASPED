e6bbe696e664e1326e7e9bffe3488811a5545618
BLANCASOTO <1715110171@utec-tgo.edu.mx>
2017-08-17 15:38:37 -0500

    Create cambiar_contrasena.php

---
+++ cambiar_contrasena.php
@@ -0,0 +1,34 @@
+<?php
+  
+  //se almacenan las variables a insertar
+  $telefono = $_POST['telefono'];
+  $contrasena = $_POST['contrasena'];
+  
+  //llamar mysql-login.php que contiene los datos de la base de datos para conectar
+  require_once ('mysql-login.php');
+ 
+ //ejecucion de conexion o devolucion de error
+  $conexion = mysqli_connect($server, $user, $pass,$bd);;
+  if (!$conexion) {
+    die('Connect Error: '.mysqli_connect_error());
+  }
+  
+  //estándar de codificación Unicode Transformation 8 bits para compatibilidad ASCII
+  mysqli_set_charset($conexion, "utf8");
+
+//encriptar contrasena
+$contrasena = md5($contrasena);
+
+//consulta actualizacion de contraseña
+$query_act_cont = "update personal set contrasena = '$contrasena' where fk_telefono =
+(select id_telefono from telefonos where concat(fk_lada,telefono) = '$telefono')";
+
+//ejecutar la consulta de actualizacion de contraseña
+$result_act_cont = mysqli_query($conexion, $query_act_cont) or die('result_act_cont Error:'.mysqli_error());
+  
+//cerrar conexion
+  mysqli_close($conexion)or die("Error en desconexion");
+//impresion de true en caso exitoso
+  echo "true";
+  
+?>
